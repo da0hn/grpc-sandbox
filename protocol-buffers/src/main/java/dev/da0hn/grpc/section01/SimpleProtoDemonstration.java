3b01e1391c63651728daaa854b147537a2e628f5
Gabriel Honda <gabrielhondacba@gmail.com>
2024-10-08 19:22:21 -0400

    feat: adiciona exemplo de criação de uma instância definida pelo `.proto`

---
+++ protocol-buffers/src/main/java/dev/da0hn/grpc/section01/SimpleProtoDemonstration.java
@@ -0,0 +1,23 @@
+package dev.da0hn.grpc.section01;
+
+import dev.da0hn.grpc.proto.models.PersonOuterClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SimpleProtoDemonstration {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(SimpleProtoDemonstration.class);
+
+  public static void main(String[] args) {
+
+    final var johnDoe = PersonOuterClass.Person.newBuilder()
+      .setName("John Doe")
+      .setAge(21)
+      .build();
+
+
+    LOGGER.info("{}", johnDoe);
+
+  }
+
+}
