238683d5d991de9f95c2611958c44be75961554a
Gabriel Honda <gabrielhondacba@gmail.com>
2025-09-28 19:39:36 -0400

    feat: adiciona teste assíncrono para listagem de contas
    - Cria o teste `test3` para verificar a listagem assíncrona de contas utilizando `StreamObserver`.
    - Atualiza a classe `AbstractTest` para incluir o `asyncStub` compartilhado entre os testes.
    - Insere logs no teste assíncrono para facilitar o rastreamento do fluxo de execução.

--- gRPC-unary-communication/src/test/java/dev/da0hn/grpc/unary/communication/section06/AbstractTest.java
+++ gRPC-unary-communication/src/test/java/dev/da0hn/grpc/unary/communication/section06/AbstractTest.java
@@ -11,11 +11,13 @@ public abstract class AbstractTest extends AbstractChannelTest {
     private final GrpcServer grpcServer = GrpcServer.create(new BankService());
 
     protected BankServiceGrpc.BankServiceBlockingStub blockingStub;
+    protected BankServiceGrpc.BankServiceStub asyncStub;
 
     @BeforeAll
     public void setup() {
         this.grpcServer.start();
         this.blockingStub = BankServiceGrpc.newBlockingStub(this.channel);
+        this.asyncStub = BankServiceGrpc.newStub(this.channel);
     }
 
     @AfterAll
--- gRPC-unary-communication/src/test/java/dev/da0hn/grpc/unary/communication/section06/UnaryCommunicationTest.java
+++ gRPC-unary-communication/src/test/java/dev/da0hn/grpc/unary/communication/section06/UnaryCommunicationTest.java
@@ -1,12 +1,21 @@
 package dev.da0hn.grpc.unary.communication.section06;
 
+import com.google.protobuf.Empty;
+import dev.da0hn.grpc.proto.section06.Accounts;
 import dev.da0hn.grpc.proto.section06.BalanceCheckRequest;
 import dev.da0hn.grpc.unary.communication.section06.repository.AccountRepository;
+import io.grpc.stub.StreamObserver;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
 
 class UnaryCommunicationTest extends AbstractTest {
+
+    private static final Logger log = LoggerFactory.getLogger(UnaryCommunicationTest.class);
 
     @Test
     @DisplayName("Should get the account balance for a given account number")
@@ -23,9 +32,36 @@ class UnaryCommunicationTest extends AbstractTest {
     @Test
     @DisplayName("Should get all accounts")
     void test2() {
-        final var response = this.blockingStub.getAllAccounts(com.google.protobuf.Empty.newBuilder().build());
+        final var response = this.blockingStub.getAllAccounts(Empty.newBuilder().build());
         Assertions.assertThat(response.getAccountsList()).isNotEmpty();
         Assertions.assertThat(response.getAccountsCount()).isEqualTo(AccountRepository.ACCOUNTS_QUANTITY);
+    }
+
+    @Test
+    @DisplayName("Should get all accounts asynchronously")
+    void test3() throws InterruptedException {
+        final var latch = new CountDownLatch(1);
+        this.asyncStub.getAllAccounts(
+            Empty.getDefaultInstance(), new StreamObserver<>() {
+                @Override
+                public void onNext(final Accounts value) {
+                    log.info("Received async accounts: {}", value);
+                    Assertions.assertThat(value.getAccountsList()).isNotEmpty();
+                    Assertions.assertThat(value.getAccountsCount()).isEqualTo(AccountRepository.ACCOUNTS_QUANTITY);
+                }
+
+                @Override
+                public void onError(final Throwable t) {
+                    Assertions.fail(t);
+                }
+
+                @Override
+                public void onCompleted() {
+                    latch.countDown();
+                }
+            }
+        );
+        latch.await();
     }
 
 }
