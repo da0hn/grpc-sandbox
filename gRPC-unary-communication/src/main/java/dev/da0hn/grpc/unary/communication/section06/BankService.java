f72c6bc94d505c1de3f67e6178fd505fe6ccf57e
Gabriel Honda <gabrielhondacba@gmail.com>
2025-09-28 16:41:35 -0400

    feat: randomiza saldo de conta no método `getAccountBalance`
    - Atualiza o método `getAccountBalance` da classe `BankService` para usar `ThreadLocalRandom` na geração de saldo.
    - Substitui o cálculo fixo pelo uso de um valor aleatório dentro do intervalo de 1 a 25, multiplicado pelo número da conta.

--- gRPC-unary-communication/src/main/java/dev/da0hn/grpc/unary/communication/section06/BankService.java
+++ gRPC-unary-communication/src/main/java/dev/da0hn/grpc/unary/communication/section06/BankService.java
@@ -5,13 +5,15 @@ import dev.da0hn.grpc.proto.section06.BalanceCheckRequest;
 import dev.da0hn.grpc.proto.section06.BankServiceGrpc;
 import io.grpc.stub.StreamObserver;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
 
     @Override
     public void getAccountBalance(final BalanceCheckRequest request, final StreamObserver<AccountBalance> responseObserver) {
         final var accountBalance = AccountBalance.newBuilder()
             .setAccountNumber(request.getAccountNumber())
-            .setBalance(request.getAccountNumber() * 10)
+            .setBalance(request.getAccountNumber() * ThreadLocalRandom.current().nextInt(1, 25))
             .build();
         responseObserver.onNext(accountBalance);
         responseObserver.onCompleted();
